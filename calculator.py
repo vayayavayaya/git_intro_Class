3ff65dc2ddbf4a77bc605e494a2d4b43a5e24103
antonygatua <antonnymuiko@gmail.com>
2025-01-07 19:58:29 +0300

    calculator functions

---
+++ calculator.py
@@ -0,0 +1,22 @@
+def add_numbers(a, b):
+    return a + b
+
+def sub_numbers(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    """
+    This function performs division of two numbers.
+
+    parameter:
+        a (float): The dividend
+        b (float): The divisor
+
+    returns:
+        float: The quotient of a and b
+    """
+    return a / b
+
